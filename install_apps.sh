afd73e7c74212f60fbf8077d76368c2ce9a92ce0
jon walker <jonathan.b.walker0@gmail.com>
2018-07-08 13:47:29 -0700

    config new mac

---
+++ install_apps.sh
@@ -0,0 +1,112 @@
+# Bootstrap script for installing apps on a fresh macOS machine
+# https://gist.github.com/codeinthehole/26b37efa67041e1307db
+
+# Check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+brew update
+
+# Install GNU core utilities (those that come with OS X are outdated)
+#brew tap homebrew/dupes
+#brew install coreutils
+#brew install gnu-sed --with-default-names
+#brew install gnu-tar --with-default-names
+#brew install gnu-indent --with-default-names
+#brew install gnu-which --with-default-names
+#brew install gnu-grep --with-default-names
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+# Install Bash 4
+brew install bash
+
+PACKAGES=(
+    git
+    lynx
+    markdown
+    npm
+    yarn
+    terminal-notifier
+    vim
+    wget
+    zsh
+    zsh-completions
+)
+
+echo "Installing packages..."
+brew install ${PACKAGES[@]}
+
+echo "Cleaning up..."
+brew cleanup
+
+echo "Installing cask..."
+brew install caskroom/cask/brew-cask
+
+CASKS=(
+    firefox
+    google-chrome
+    iterm2
+    macvim
+    skype
+    slack
+    spotify
+    sublime-text
+    spectacle
+    visual-studio-code
+    webstorm
+)
+
+echo "Installing cask apps..."
+brew cask install ${CASKS[@]}
+
+echo "Installing fonts..."
+brew tap caskroom/fonts
+FONTS=(
+    font-inconsolidata
+    font-roboto
+    font-clear-sans
+)
+brew cask install ${FONTS[@]}
+
+#echo "Installing Ruby gems"
+#RUBY_GEMS=(
+#    bundler
+#    filewatcher
+#    cocoapods
+#)
+#sudo gem install ${RUBY_GEMS[@]}
+
+echo "Installing global npm packages..."
+npm install marked -g
+
+echo "Configuring OSX..."
+
+# Require password as soon as screensaver or sleep mode starts
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Show filename extensions by default
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Show hidden files by default 
+defaults write com.apple.finder AppleShowAllFiles YES
+
+# Enable tap-to-click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+echo "Creating folder structure..."
+[[ ! -d Wiki ]] && mkdir Wiki
+[[ ! -d Workspace ]] && mkdir Workspace
+
+if test ! $(which zsh); then
+    echo "Installing oh my zsh..."
+    ruby -e "$(curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh)"
+fi
+
+echo "Bootstrapping complete"
