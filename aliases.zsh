f2c1ec5336b66629e17b18999e92afb0eb99bfd1
jon walker <jonathan.b.walker0@gmail.com>
2018-07-08 15:17:40 -0700

    aliases

---
+++ aliases.zsh
@@ -0,0 +1,21 @@
+#system aliases
+alias ip='ipconfig getifaddr en0'
+alias wip='ipconfig getifaddr en1'
+
+#git aliases
+alias gc='git checkout $1'
+alias gstat='git status -s'
+alias gad='git add -A'
+alias dev="git checkout develop"
+alias master="git checkout master"
+
+#java aliases
+alias javavs="/usr/libexec/java_home -V"
+alias javav="java -version"
+
+#docker aliases
+alias dps="docker ps"
+alias dup="docker-compose up"
+alias ddown="docker-compose down"
+alias dpull="docker-compose pull"
+alias dall=" docker-compose down --rmi ‘all’"
