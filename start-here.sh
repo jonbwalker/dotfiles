f3fe434f9e512ef13bc9c357c215b89d2f084eff
jon walker <jonathan.b.walker0@gmail.com>
2018-07-09 13:06:19 -0700

    ask to run app install script

--- start-here.sh
+++ start-here.sh
@@ -17,3 +17,22 @@ echo "Clone dotfiles from Github"
 cd $HOME/Workspace
 git clone https://github.com/jonbwalker/dotfiles.git
 
+ask_for_confirmation "Are you ready to start the main install?"
+    if yes; then
+        sh install_apps.sh
+    else
+         # Print output in red
+         printf "\e[0;31m  [✖] install cancelled\e[0m\n"
+    fi
+
+ask_for_confirmation() {
+    print_question "$1 (y/n) "
+    read -n 1
+    printf "\n"
+}
+
+yes() {
+    [[ "$REPLY" =~ ^[Yy]$ ]] \
+        && return 0 \
+        || return 1
+}
