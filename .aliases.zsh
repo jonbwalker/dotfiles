3a846c40a7013dae1e536f4d2b1f2cf94a98a1dc
jonbwalker <jonathan.b.walker0@gmail.com>
2020-01-10 08:48:14 -0800

    use single quotes because double broke gcm

--- .aliases.zsh
+++ .aliases.zsh
@@ -3,83 +3,81 @@ alias ip='ipconfig getifaddr en0'
 alias pip='curl https://ipinfo.io/ip'
 alias wip='ipconfig getifaddr en1'
 alias indexsl='sudo mdutil -E /'
-alias ref=". ~/.zshrc"
-alias debug="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222"
+alias ref='. ~/.zshrc'
+alias debug='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222'
 
 #git aliases
-alias gc="git checkout $1"
-alias gstat='git status -s'
+alias gc='git checkout $1'
+alias gcm='gc master'
+alias gpr='git pull --rebase'
+alias st='git status -s'
 alias gad='git add -A'
-alias dev="git checkout develop"
-alias master="git checkout master"
-alias grs="git reset --soft HEAD^"
-alias fbranch="gup && gcb"
-alias branch="sl && gcf && fbranch"
-alias gpm="git pull origin master"
+alias grs='git reset --soft HEAD^'
+alias fbranch='gup && gcb'
+alias branch='sl && gcf && fbranch'
+alias gpm='git pull origin master'
 
 #java aliases
-alias javavs="/usr/libexec/java_home -V"
-alias javav="java -version"
+alias javavs='/usr/libexec/java_home -V'
+alias javav='java -version'
 
 # shortcuts
-alias dfiles="cd ~/Workspace/dotfiles"
+alias dfiles='cd ~/Workspace/dotfiles'
 alias vzsh='vim ~/.zshrc'
-alias jbw="~//Workspace/jonbwalker.com"
+alias jbw='~//Workspace/jonbwalker.com'
 
 #npm global aliaes
-alias ni="npm i"
-alias nrm="rm -rf node_modules && ni"
+alias ni='npm i'
+alias nrm='rm -rf node_modules && ni'
 
 #npx aliases
-alias ntm="npx majestic --app"
+alias ntm='npx majestic --app'
 
 #java aliases
-alias javavs="/usr/libexec/java_home -V"
-alias javav="java -version"
+alias javavs='/usr/libexec/java_home -V'
+alias javav='java -version'
 
 #sendlane aliases
-alias hs="cd ~/Homestead"
-alias sl="cd ~/code/sendlane/sendlane"
-alias sli="cd ~/code/sendlane/sendlane/resources/assets/mix/interface/views"
-alias gcm="gc master"
-alias gca="gc feature/automation-builder"
-alias nh="npm run hot"
-alias nd="npm run dev"
-alias nho="npm run hot:ops"
-alias nw="build=interface npm run watch"
-alias nsd="build=scripts npm run dev"
-alias nsw="build=static npm run watch"
+alias hs='cd ~/Homestead'
+alias sl='cd ~/code/sendlane/sendlane'
+alias sli='cd ~/code/sendlane/sendlane/resources/assets/mix/interface/views'
+alias nh='npm run hot'
+alias nd='npm run dev'
+alias nho='npm run hot:ops'
+alias nw='build=interface npm run watch'
+alias nsd='build=scripts npm run dev'
+alias nsw='build=static npm run watch'
 alias na='npn run dev-all'
-alias ntw="npm run test:watch"
-alias ntc="npm run test:coverage"
+alias ntw='npm run test:watch'
+alias ntc='npm run test:coverage'
 
 #php aliases
-alias art="sl && php artisan"
-alias rseed="cda && art db:seed --class="
-alias ac="art config:cache"
-alias acc="art config:clear"
-alias bc="ca && cc && acc"
+alias art='sl && php artisan'
+alias rseed='cda && art db:seed --class='
+alias ac='art config:cache'
+alias acc='art config:clear'
+alias bc='ca && cc && acc'
 
 #php artisan commands
-alias mmain="art migrate --path=database/migrations/main"
-alias mtenant="art migrate:tenants --tenantdb _user"
-alias mtrollback="art migrate:tenants_rollback"
-alias formq="php artisan queue:listen --queue=forms"
-alias massD="php artisan massDelete:subscribers"
+alias mmain='art migrate --path=database/migrations/main'
+alias mtenant='art migrate:tenants --tenantdb _user'
+alias mtrollback='art migrate:tenants_rollback'
+alias formq='php artisan queue:listen --queue=forms'
+alias massD='php artisan massDelete:subscribers'
 source ~/.sendlane-profile
 
 #composer aliases
 alias ca='composer dump-autoload'
-alias ci="composer install"
-alias cu="composer update"
-alias cc="composer clear-cache"
+alias ci='composer install'
+alias cu='composer update'
+alias cc='composer clear-cache'
 
 #docker aliases
-alias dc="docker-compose"
-alias dps="docker ps"
-alias dup="dc up"
-alias ddown="dc down"
-alias dpull="dc pull"
+alias dc='docker-compose'
+alias dps='docker ps'
+alias dup='dc up'
+alias ddown='dc down'
+alias dpull='dc pull'
 alias dall=" dc down --rmi ‘all’"
-alias dlogs="docker logs $1"
-alias dexec="docker exec -it $1 /bin/bash"
+alias dlogs='docker logs $1'
+alias dexec='docker exec -it $1 /bin/bash'
