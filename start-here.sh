980d7e983dee6c833ada573d0eafc26870de0e83
jon walker <jonathan.b.walker0@gmail.com>
2020-12-01 19:42:52 -0800

    update bash install

--- start-here.sh
+++ start-here.sh
@@ -6,7 +6,7 @@
 # Install Homebrew
 if test ! $(which brew); then
     echo "Installing homebrew..."
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Update homebrew recipes
